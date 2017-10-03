19b291176e339f607bfe6be0294208d5aeddd1cf
Vedlct <mujtaba.rumi1@gmail.com>
2017-10-03 09:07:58 +0600

    rumi

--- dev/website/application/models/Coursem.php
+++ dev/website/application/models/Coursem.php
@@ -4,12 +4,20 @@
 class Coursem extends CI_Model
 {
 
-    public function getCourseForTerms(){
+    public function getCourseTitle(){
 
-        $this->db->select('courseId,courseTitle,courseImage,academicYear');
+        $this->db->select('courseId,courseTitle,courseImage,academicYear, departmentId');
         $query = $this->db->get('ictmcourse');
         return $query->result();
 
+    }
+
+    public function getCourseList(){
+        $this->db->select('courseId,courseTitle,courseImage,academicYear,departmentId,departmentName');
+        $this->db->where('courseStatus', STATUS[0]);
+        $this->db->join('ictmdepartment', 'ictmdepartment.departmentId = ictmcourse.departmentId','left');
+        $query = $this->db->get('ictmcourse');
+        return $query->result();
     }
 
 
--- dev/website/application/views/test.php
+++ dev/website/application/views/test.php
@@ -1,30 +1,11 @@
 <?php
-//
-//foreach ($mainmenu as $mn) {
-//    echo $mn->menuName."<br>";
-//
-//        foreach ($parentmenu as $pm) {
-//            if ($pm->parentId != "") {
-//            echo $pm->menuName . "<br>";
-//        }
-//
-//    }
-//}
-//?>
 
-<?php
-foreach ($mainmenu as $mn) {
-    echo $id = $mn->menuId;
-    echo $mn->menuName."<br>";
-
-    $this->db->select('menuId, menuName, parentId ');
-    $this->db->where('menuType', MENU_TYPE[1]);
-    $this->db->where('menuStatus', STATUS[0]);
-    $this->db->where('parentId =', $id);
-    $query = $this->db->get('ictmmenu');
-    foreach ($query->result() as $q) {
-        //echo $q->menuName."<br>";
+foreach ($departmentname as $dp){
+    echo $dp->departmentName."<br>";
+    foreach ($coourselist as $cl){
+        if ( $cl->departmentId == $dp->departmentId ){
+            echo $cl->courseTitle."<br>";
+        }
     }
-
 }
 ?>
