526eb80f328ac0ac5e62fa674806058e55e71417
rumi <mujtaba.rumi1@gmail.com>
2018-01-18 16:46:54 +0600

    rumi

--- dev/AdminPanel/application/models/Admin/RegisterInterestm1.php
+++ dev/AdminPanel/application/models/Admin/RegisterInterestm1.php
@@ -26,18 +26,28 @@ class RegisterInterestm1 extends CI_Model
             if($_POST['search']['value']) // if datatable send POST for search
             {
 
-                if($i===0) // first loop
+//                if($i===0) // first loop
+//                {
+//                    $this->db->group_start(); // open bracket. query Where with OR clause better with bracket. because maybe can combine with other WHERE with AND.
+//                    $this->db->like($item, $_POST['search']['value']);
+//                }
+//                else
+//                {
+//                    $this->db->or_like($item, $_POST['search']['value']);
+//                }
+//
+//                if(count($this->column_search) - 1 == $i) //last loop
+//                    $this->db->group_end(); //close bracket
+
+                $searchbox = explode(' ', $_POST['search']['value']);
+                $where = array();
+                foreach($searchbox as $word)
                 {
-                    $this->db->group_start(); // open bracket. query Where with OR clause better with bracket. because maybe can combine with other WHERE with AND.
-                    $this->db->like($item, $_POST['search']['value']);
-                }
-                else
-                {
-                    $this->db->or_like($item, $_POST['search']['value']);
+                    $where[] = "(firstName LIKE '%".$word."%' OR surName LIKE '%".$word."%')";
                 }
 
-                if(count($this->column_search) - 1 == $i) //last loop
-                    $this->db->group_end(); //close bracket
+                $this->db->where(implode(' AND ', $where));
+
             }
             $i++;
         }
