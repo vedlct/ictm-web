a39d2c98d303654d6a133551abeede03b958235c
Vedlct <mujtaba.rumi1@gmail.com>
2018-01-20 03:23:09 +0600

    rumi

--- dev/AdminPanel/application/views/Admin/head.php
+++ dev/AdminPanel/application/views/Admin/head.php
@@ -10,6 +10,8 @@
 <!-- Bootstrap CSS -->
 
 <link rel="stylesheet" href="<?php echo base_url()?>public/css/bootstrapV3.3.7.min.css" rel="stylesheet"/>
+<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">-->
+
 <!-- bootstrap theme -->
 <link href="<?php echo base_url()?>public/css/bootstrap-theme.css" rel="stylesheet">
 <!--external css-->
