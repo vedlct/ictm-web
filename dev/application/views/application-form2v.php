5120e4806bc486feef2de3aae6ea3e84a294e470
mdsak <md.sakibrahman@hotmail.com>
2019-08-13 13:35:29 +0600

    sakib

--- dev/application/views/application-form2v.php
+++ dev/application/views/application-form2v.php
@@ -118,7 +118,7 @@ elseif($this->session->flashdata('successMessage')!=null){?>
                                 <a href="<?php echo base_url()?>Apply" ><button type="button"  class="btn btn-previous">Previous</button></a>
                                 <button type="submit" class="btn btn-next">Save Application</button>
 <!--                                <button type="submit" formaction="--><?php //echo base_url()?><!--ApplyOnline/editORInsertApplicationForm2AndNext" class="btn btn-next">Save And Next</button>-->
-                                <a href="<?php echo base_url()?>Apply-Work-Experience" ><button type="button"  class="btn ">Next</button></a>
+                                <a href="<?php echo base_url()?>Apply-Work-Experience" ><button type="button"  class="btn btn-next">Next</button></a>
                             </div>
                         </div>
                         <!--                    </fieldset>-->
